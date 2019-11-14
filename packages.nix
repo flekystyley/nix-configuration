46adafa13bfe86067bd884459c69c0b4cfaec4d8
flekystyley <flekystyley@gmail.com>
2019-11-15 00:53:34 +0900

    update packages.nix;

--- packages.nix
+++ packages.nix
@@ -8,58 +8,59 @@
  environment.systemPackages = with pkgs; [
 
 	# basic
-    wget 
-    curl
-    git
-    zsh 
-    gcc 
-    gnumake 
-    binutils 
-    pkg-config 
-    zip 
-    unzip 
-    xclip 
-    htop
-    openal 
-    byobu 
-    xorg.xev 
-    xorg.xprop
-    evince 
-    tig
-    peco
-    neofetch
-    direnv
-    dmenu
-    compton
-    qemu
+        wget 
+        curl
+        git
+        zsh 
+        gcc 
+        gnumake 
+        binutils 
+        pkg-config 
+        zip 
+        unzip 
+        xclip 
+        htop
+        openal 
+        byobu 
+        xorg.xev 
+        xorg.xprop
+        evince 
+        tig
+        peco
+        neofetch
+        direnv
+        dmenu
+        compton
+        qemu
 	tmux
+	ghq
 
 	# terminal
-    terminator
+        terminator
 
 	# browser
 	chromium
 
 	# editor
-    emacs
-    vim
+        emacs
+        vim
 	vscode
 
 	# programming launguage 
-    go
-    gcc
-    libcap
+        go
+        gcc
+        libcap
 
 	# file manager
-    ranger
+        ranger
 
 	# desktop manager
 	feh
 
-    # editor
-    emacs
-    vim 
-    vscode 
+        # editor
+        emacs
+        vim 
+        vscode 
   ];
 
   environment.variables = {
